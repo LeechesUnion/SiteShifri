a93b2d75124d44e092fd31e8ba6fab67dc04b011
Ekaterina Shevchenko <75583677+ShevchenkoKate@users.noreply.github.com>
2022-12-09 23:54:36 +0300

    Create sub_js.js

---
+++ js/sub_js.js
@@ -0,0 +1,15 @@
+var position = 0;
+function stop() {
+    position = 0;
+    setTimeout(scroller, 3000)
+}
+function scroller() {
+
+    if (position != 3230) {
+        position++;
+        scroll(0, position);
+        clearTimeout(timer);
+        var timer = setTimeout("scroller()", 7); timer;
+    }
+    else { location.replace(); }
+}
